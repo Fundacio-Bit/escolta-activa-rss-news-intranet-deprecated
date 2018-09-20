4c823c7157b36e32b4cd6913768f05a64db46213
Oscar Moya <omoya@fundaciobit.org>
2018-09-20 13:58:38 +0200

    The list of documents (news) is not retrieved at the App level now Components have been renamed

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react';
-import RSSList from './RSSList';
-import $ from 'jquery';
+import FilterableSourcesTable from './filterable-sources-table';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      retrievedDocuments: []  
-    };
-  }
-
-
- // Callback that ensures that teh API calls done by this component are executed once it is mounted. 
-  componentDidMount() {
-    this.retrievedDocumentsList();
-  }
-
- // Call the REST API to get all docuements
-  retrievedDocumentsList() {
-    $.getJSON('http://localhost:8000/rss-news/entries')
-      .then(({results}) => this.setState({retrievedDocuments: results}));      
-  }
-
   // Render the results obtained by the API call (using JSX syntax). 
   render() {
     return (
       <div>
-        <RSSList retrievedDocs={this.state.retrievedDocuments} />
+        <FilterableSourcesTable/>
       </div>
     );
   }
