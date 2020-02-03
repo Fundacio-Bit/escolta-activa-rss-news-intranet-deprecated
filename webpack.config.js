811944020273451f2f777f1100569b17599a041c
omoya_fbit <omoya@fundaciobit.org>
2020-02-03 10:52:51 +0100

    Fixed some commands to improve source mpas creation process

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
@@ -6,12 +7,23 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   // Options for devtool at:
   // https://webpack.js.org/configuration/devtool/
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   entry: __dirname + "/src/index.js",
   output: {
     path: __dirname + "/build",
     filename: "bundle.js"
   },
+
+  // https://blog.sentry.io/2015/10/29/debuggable-javascript-with-source-maps
+  // devtool: "source-map",
+  // entry: {
+  //   "bundle": path.join(__dirname, 'src/index.js')
+  // },
+  // output: {
+  //   path: path.join(__dirname, 'build'),
+  //   filename: "[name].js",
+  //   sourceMapFilename: "[name].js.map"
+  // },
 
   module: {
     loaders: [
@@ -23,9 +35,13 @@ module.exports = {
           presets: ['es2015', 'react', 'stage-2']
         }
       },
+      // {
+      //   test: /\.css$/,
+      //   loader: ExtractTextPlugin.extract('style', 'css')
+      // },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style', 'css')
+        loader: 'style-loader!css-loader'
       },
     ]
   },
@@ -33,9 +49,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: __dirname + "/src/index.html"
     }),
-    new ExtractTextPlugin("bundle.css"),
+    // new ExtractTextPlugin("bundle.css"),
     // Descomentar para pasar a produccion
-    new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
+    // new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
     // new BundleAnalyzerPlugin(),
   ]
 }
+
+
+// solucionado quitando ExtractTextPluginLoader
