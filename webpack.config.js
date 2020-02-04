83519bb69eb95dd8841d12ca9cdaf0fbb35ff7f6
omoya_fbit <omoya@fundaciobit.org>
2020-02-04 12:56:45 +0100

    enabled uglifycation and CSS text extraction for production

--- webpack.config.js
+++ webpack.config.js
@@ -35,10 +35,10 @@ module.exports = {
           presets: ['es2015', 'react', 'stage-2']
         }
       },
-      // {
-      //   test: /\.css$/,
-      //   loader: ExtractTextPlugin.extract('style', 'css')
-      // },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css')
+      },
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
@@ -49,10 +49,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: __dirname + "/src/index.html"
     }),
-    // new ExtractTextPlugin("bundle.css"),
+    new ExtractTextPlugin("bundle.css"),
     // Descomentar para pasar a produccion
-    // new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
-    // new BundleAnalyzerPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
+    new BundleAnalyzerPlugin(),
   ]
 }
 
