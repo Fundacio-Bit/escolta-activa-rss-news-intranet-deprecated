e15d9c97908c50b2059c02254d9a678c5048a35a
omoya_fbit <omoya@fundaciobit.org>
2020-04-30 10:03:45 +0200

    styling, interactivity and content improvements

--- src/components/topics-news.js
+++ src/components/topics-news.js
@@ -1,23 +1,23 @@
 /* eslint-disable no-prototype-builtins */
 // import React, { Component } from 'react';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Paper from '@material-ui/core/Paper';
-import CircularProgress from '@material-ui/core/CircularProgress'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Paper from "@material-ui/core/Paper";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-import ErrorIcon from '@material-ui/icons/Error'
-import { baseErrorMessage, getErrorMessage } from './utils/getErrorMessage.js'
-import { makeStyles } from '@material-ui/core/styles'
+import ErrorIcon from "@material-ui/icons/Error";
+import { baseErrorMessage, getErrorMessage } from "./utils/getErrorMessage.js";
+import { makeStyles } from "@material-ui/core/styles";
 
-import PropTypes from 'prop-types';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import { deepOrange, deepPurple } from '@material-ui/core/colors';
+import PropTypes from "prop-types";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import { deepOrange, deepPurple, red } from "@material-ui/core/colors";
+import { NonceProvider } from "react-select";
 
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     // height: '100vmax',
     paddingTop: 30,
@@ -27,30 +27,34 @@ const useStyles = makeStyles(theme => ({
     // minWidth: '20%',
     // maxWidth: '35%',
     backgroundColor: theme.palette.background.paper,
+    border: "#e3dee3",
+    borderWidth: "2px",
+    borderStyle: "solid",
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
   },
   rank1: {
     fontSize: 50,
-    textAlign: 'left',
+    textAlign: "left",
   },
   topTopic: {
     fontSize: 50,
-    textAlign: 'left',
+    textAlign: "left",
   },
   topicNew: {
-    textAlign: 'left',
+    textAlign: "left",
+    textDecoration: "none",
   },
   error: {
     marginTop: theme.spacing(1),
-    color: '#e91e63'
+    color: "#e91e63",
   },
   loading: {
     flexGrow: 1,
-    marginTop: theme.spacing(4)
+    marginTop: theme.spacing(4),
   },
   orange: {
     color: theme.palette.getContrastText(deepOrange[500]),
@@ -59,57 +63,61 @@ const useStyles = makeStyles(theme => ({
   purple: {
     color: theme.palette.getContrastText(deepPurple[500]),
     backgroundColor: deepPurple[500],
-  }
+  },
+  tagsContainer: {
+    fontWeight: 200,
+    marginLeft: "20px",
+    border: "#e3dee3",
+    borderWidth: "2px",
+    borderStyle: "solid",
+    backgroundColor: deepOrange[500],
+    background: deepOrange[500],
+  },
 }));
 
 export const TopicsNews = (props) => {
-
-  const classes  = useStyles();
+  const classes = useStyles();
 
   function renderTopic(props) {
     const { index, style } = props;
-  
-    return (      
+
+    return (
       <ListItem button style={style} key={index}>
         <ListItemAvatar>
-          <Avatar className={index > 3 ? classes.orange: classes.purple} >
+          <Avatar className={index > 3 ? classes.orange : classes.purple}>
             {index + 1}
           </Avatar>
-      </ListItemAvatar>
+        </ListItemAvatar>
         <ListItemText primary={`${data[index].name} ${data[index].count}`} />
       </ListItem>
     );
   }
-  
+
   renderTopic.propTypes = {
     index: PropTypes.number.isRequired,
     style: PropTypes.object.isRequired,
   };
 
   return (
-    <div className={classes.root}>
-      { data.length > 0 && data.map(u => {
-              return (                    
-                <a href={u.link} className={classes.topicNew}  target="_blank"><h3>{u.title}</h3></a>
-              );
-            }
-          )
-        };
+    <div className={classes.tagsContainer}>
+      {data.length > 0 &&
+        data.map((u) => {
+          return (
+            <a href={u.link} className={classes.topicNew} target="_blank">
+              <h3>{u.title}</h3>
+            </a>
+          );
+        })}
+      ;
     </div>
   );
-}
+};
 
-
-
-  // https://material-ui.com/es/components/avatars/
-  // https://material-ui.com/es/components/chips/
-  // https://material-ui.com/es/components/lists/
-  // https://material-ui.com/es/components/typography/
-  // https://material-ui.com/es/customization/typography/
-
-
-
-
+// https://material-ui.com/es/components/avatars/
+// https://material-ui.com/es/components/chips/
+// https://material-ui.com/es/components/lists/
+// https://material-ui.com/es/components/typography/
+// https://material-ui.com/es/customization/typography/
 
 // {!loading && !errorStatus.error &&
 //   <Paper className={classes.root}>
@@ -123,14 +131,14 @@ export const TopicsNews = (props) => {
 //       )}
 //       <TableBody>
 //       {filteredData.length > 0 && filteredData.map(u => {
-//         return (                    
+//         return (
 //           <TopicsTableRow
 //             key= { u.name }
 //             topic = { u.name }
 //             count = { u.count }
 //           />
 //         );
-//       })} 
+//       })}
 //       {filteredData.length === 0 && (
 //         <TableRow>
 //           <TableCell colSpan={4}>
@@ -143,8 +151,7 @@ export const TopicsNews = (props) => {
 //         </TableRow>
 //       )}
 //       </TableBody>
-//     </Table>          
+//     </Table>
 //   </div>
 // </Paper>
 // }
-
--- src/components/topics.js
+++ src/components/topics.js
@@ -1,154 +1,293 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import { TopicsRanking } from './topics-ranking';
-import { TopicsNews } from './topics-news';
-import TopicsSearchAppBar from './topics-search-app-bar';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import { baseErrorMessage, getErrorMessage } from './utils/getErrorMessage.js'
-import { makeStyles } from '@material-ui/core/styles'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+import { TopicsRanking } from "./topics-ranking";
+import TopicsSearchAppBar from "./topics-search-app-bar";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { baseErrorMessage, getErrorMessage } from "./utils/getErrorMessage.js";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     // flexGrow: 1,
     // width: '100%',
     // height: '100%',
   },
-  paper: {
-    // padding: theme.spacing(y) * 2,
-    textAlign: 'center',
+  title: {
+    textAlign: "left",
+    flexBasis: "15%",
+    marginBottom: "1em",
+  },
+  topicsPaper: {
+    textAlign: "center",
     color: theme.palette.text.secondary,
-  },  
+    boxShadow:
+      "0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
+  },
+  topicsGridxs12: {
+    display: "flex",
+    padding: "unset",
+    flexBasis: "100%",
+  },
+  topicsGridSection: {
+    boxShadow:
+      "0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
+    margin: "20px 0px 0px 0px",
+    width: "100%",
+    padding: "unset",
+  },
+  topicsGridContainer: {
+    margin: "20px 0px 0px 0px",
+    width: "100%",
+    padding: "unset",
+    flexBasis: "100%",
+  },
+  rankingsPanel: {
+    flexBasis: "34%",
+    boxShadow:
+      "0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
+    margin: "20px 0px 0px 0px",
+    width: "100%",
+    padding: "unset",
+  },
+  newsPanel: {
+    flexBasis: "64%",
+    boxShadow:
+      "0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
+    margin: "20px 0px 0px 0px",
+    width: "100%",
+    padding: "unset",
+  },
+  gridDivider: {
+    flexBasis: "2%",
+    margin: "20px 0px 0px 0px",
+    width: "100%",
+    padding: "unset",
+  },
+  test: {
+    boxShadow:
+      "0px 2px 1px -1px rgba(0,0,0,0.4), 0px 1px 1px 0px rgba(0,0,0,0.28), 0px 1px 7px 0px rgba(0,0,0,0.24)",
+    height: "100%",
+    margin: "1em",
+    padding: "1em",
+  },
+  test2: {
+    padding: "unset!important",
+    paddingBottom: "10px!important",
+    width: "100%",
+    margin: "unset",
+  },
+  test3: {
+    boxShadow:
+      "0px 3px 2px -2px rgba(0,0,0,0.4), 0px 2px 2px 0px rgba(0,0,0,0.28), 0px 2px 5px 0px rgba(0,0,0,0.24)",
+    height: "100%",
+  },
+  test4: {
+    boxShadow:
+      "0px 2px 1px -1px rgba(0,0,0,0.4), 0px 1px 1px 0px rgba(0,0,0,0.28), 0px 1px 7px 0px rgba(0,0,0,0.24)",
+    margin: "1em",
+    padding: "1em",
+  },
 }));
 
 export const Topics = (props) => {
-
   var today = new Date();
-  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
   var yyyy = today.getFullYear();
-  var year_month_str = yyyy + '-' + mm
-
+  var year_month_str = yyyy + "-" + mm;
 
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [errorStatus, setErrorStatus] = useState({error: false, message: ''});
+  const [errorStatus, setErrorStatus] = useState({ error: false, message: "" });
   const [selectedMonth, setSelectedMonth] = useState(year_month_str);
+  const [selectedTopic, setSelectedTopic] = useState("");
 
-  const classes  = useStyles();
+  const classes = useStyles();
+
+  const handleTopicClick = (topicName) => {
+    setSelectedTopic(topicName);
+  };
 
   function renderTopic(props) {
     const { index, style } = props;
-  
-    return (      
+
+    return (
       <ListItem button style={style} key={index}>
         <ListItemAvatar>
-          <Avatar className={index > 3 ? classes.orange: classes.purple} >
+          <Avatar className={index > 3 ? classes.orange : classes.purple}>
             {index + 1}
           </Avatar>
-      </ListItemAvatar>
+        </ListItemAvatar>
         <ListItemText primary={`${data[index].name} ${data[index].count}`} />
       </ListItem>
     );
   }
-  
+
   renderTopic.propTypes = {
     index: PropTypes.number.isRequired,
     style: PropTypes.object.isRequired,
   };
 
-
-  function filterByTopic(pressNew) {
+  const filterByTopic = (pressNew) => {
     if (
-      pressNew.hasOwnProperty("topics") && pressNew.topics.toLowerCase().split(",").includes(props.searchTerm.toLowerCase())){
-        return true
-      }
-    else return false
+      pressNew.hasOwnProperty("topics") &&
+      pressNew.topics
+        .toLowerCase()
+        .split(",")
+        .includes(selectedTopic.toLowerCase())
+    ) {
+      return true;
+    } else return false;
   };
 
-  let filteredData = data.slice()
-
+  let filteredData = selectedTopic == "" ? data : data.filter(filterByTopic);
 
   // Get the news related to a topic.
-  useEffect(() => {   
-  
+  useEffect(() => {
     let unmounted = false;
 
     const fetchData = () => {
       if (!unmounted) {
-        setErrorStatus({error: false, message: ''});
+        setErrorStatus({ error: false, message: "" });
         setLoading(true);
       }
 
       try {
-        axios.get(`/rss-news/entries/yearmonth/${selectedMonth}`).then((results) => { 
-          if (results.data.results.length > 0) { 
-            // OK
-            // Beware with this:
-            // https://overreacted.io/a-complete-guide-to-useeffect/#each-render-has-its-own-event-handlers
-            setTimeout(() => {
+        axios
+          .get(`/rss-news/entries/yearmonth/${selectedMonth}`)
+          .then((results) => {
+            if (results.data.results.length > 0) {
+              // OK
+              // Beware with this:
+              // https://overreacted.io/a-complete-guide-to-useeffect/#each-render-has-its-own-event-handlers
+              setTimeout(() => {
+                if (!unmounted) {
+                  setErrorStatus({ error: false, message: "" });
+                  setLoading(false);
+                  setData(results.data.results);
+                }
+              }, 850);
+            } else {
+              // No data returned
               if (!unmounted) {
-                setErrorStatus({error: false, message: ''});
+                setData([]);
                 setLoading(false);
-                setData(results.data.results);
               }
-            }, 850);
-          }
-          else {
-            // No data returned
-            if (!unmounted) {      
-              setData([])
-              setLoading(false);
             }
-          }
-        }).catch(error => {
-          console.log(getErrorMessage(error));
-          setErrorStatus( { error: true, message: baseErrorMessage } );
-          setLoading(false);
-        });   
-
-      }
-      catch (error) {
+          })
+          .catch((error) => {
+            console.log(getErrorMessage(error));
+            setErrorStatus({ error: true, message: baseErrorMessage });
+            setLoading(false);
+          });
+      } catch (error) {
         if (!unmounted) {
           console.log(getErrorMessage(error));
-          setErrorStatus( { error: true, message: baseErrorMessage } );
+          setErrorStatus({ error: true, message: baseErrorMessage });
           setLoading(false);
         }
       }
-    }
+    };
 
     fetchData();
 
     // Cleanup function. useEffect uses the cleanup function to execute operations useful on component unmount.
-    return () => unmounted = true;
+    return () => (unmounted = true);
   }, [selectedMonth]);
+
+  {
+    /* <Grid container className={classes.root} spacing={2}>
+  <Grid item xs={12}>
+    <Grid container justify="center" spacing={2}>
+      {[0, 1, 2].map((value) => (
+        <Grid key={value} item>
+          <Paper className={classes.paper} />
+        </Grid>
+      ))}
+    </Grid>
+  </Grid>
+</Grid> */
+  }
 
   return (
     <div className={classes.root}>
-    <Grid container spacing={10}>
-        <Grid item xs={12}>
+      <Grid container className={classes.test2} spacing={10}>
+        <Grid item className={classes.test2} xs={12}>
           <Paper className={classes.paper}>
             <TopicsSearchAppBar
-              selectedMonth = {selectedMonth}
+              selectedMonth={selectedMonth}
               onSelectMonth={setSelectedMonth}
             />
-  
           </Paper>
         </Grid>
-        <Grid item xs={4}>
-          <TopicsRanking selectedMonth = {selectedMonth}/>
+        <Grid item className={classes.test2} xs={12}>
+          <div className={classes.test4}>
+            <div className={classes.title}>
+              <Typography variant="h6" id="tableTitle">
+                Indicadors globals ({selectedMonth})
+              </Typography>
+            </div>
+            <Grid item xs={1}>
+              Temes: 20
+            </Grid>
+            <Grid item xs={1}>
+              Notícies seleccionades: 157
+            </Grid>
+            <Grid item xs={1}>
+              Espanyols: 37
+            </Grid>
+            <Grid item xs={1}>
+              Alemanys: 12
+            </Grid>
+            <Grid item xs={1}>
+              Italians: 7
+            </Grid>
+            <Grid item xs={1}>
+              Francesos: 0
+            </Grid>
+          </div>
         </Grid>
-        <Grid item xs={8}>
-          { data.length > 0 && data.map(u => {
-                return (                    
-                  <a href={u.link} className={classes.topicNew}  target="_blank"><h3>{u.title}</h3></a>
+        <Grid item className={classes.test2} xs={4}>
+          <div className={classes.test}>
+            <div className={classes.title}>
+              <Typography variant="h6" id="tableTitle">
+                Rànquing de temes ({selectedMonth})
+              </Typography>
+            </div>
+            <TopicsRanking
+              selectedMonth={selectedMonth}
+              selectedTopic={selectedTopic}
+              topicSelect={handleTopicClick}
+            />
+          </div>
+        </Grid>
+        <Grid item className={classes.test2} xs={8}>
+          <div className={classes.test}>
+            <div className={classes.title}>
+              <Typography variant="h6" id="tableTitle">
+                {selectedTopic == ""
+                  ? "Totes les notícies del mes"
+                  : "Notícies sobre " + selectedTopic}
+              </Typography>
+            </div>
+            {filteredData.length > 0 &&
+              filteredData.map((u, index) => {
+                return (
+                  <a
+                    href={u.link}
+                    className={classes.topicNew}
+                    key={index}
+                    target="_blank"
+                  >
+                    <h3>{u.title}</h3>
+                  </a>
                 );
-              }
-            )
-          }; 
+              })}
+          </div>
         </Grid>
       </Grid>
-  </div>   
+    </div>
   );
-}
+};
