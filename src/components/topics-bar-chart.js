826b577f718e50dc63734a257fb3359fe37bdd4c
omoya_fbit <omoya@fundaciobit.org>
2020-05-05 13:29:14 +0200

    converted to functional component

--- src/components/topics-bar-chart.js
+++ src/components/topics-bar-chart.js
@@ -1,59 +1,96 @@
-import React, { Component } from "react";
-import Chart from "chart.js";
-// import classes from "./LineGraph.module.css";
+import React, { useEffect, useRef, useState } from "react";
+import Chartjs from "chart.js";
 
-export default class TopicsBarChart extends Component {
-  chartRef = React.createRef();
-  // classes = useStyles();
+const randomInt = () => Math.floor(Math.random() * (10 - 1 + 1)) + 1;
 
-  componentDidMount() {
-    const myChartRef = this.chartRef.current.getContext("2d");
+const TopicsBarChart = () => {
+  const chartContainer = useRef(null);
+  const [chartInstance, setChartInstance] = useState(null);
 
-    let gradient = myChartRef.createLinearGradient(0, 0, 0, 400);
-    gradient.addColorStop(0, "rgba(250,174,50,1)");
-    gradient.addColorStop(1, "rgba(250,174,50,0)");
+  useEffect(() => {
+    if (chartContainer && chartContainer.current) {
+      const myChartRef = chartContainer.current.getContext("2d");
 
-    new Chart(myChartRef, {
-      type: "bar",
-      data: {
-        //Bring in data
-        labels: ["Jan", "Feb", "March"],
-        datasets: [
-          {
-            label: "Sales",
-            backgroundColor: gradient,
-            data: [86, 67, 91],
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        title: { display: true, text: "notícies per mes" },
-        legend: { display: false },
-        scales: {
-          xAxes: [
+      let gradient = myChartRef.createLinearGradient(0, 0, 0, 400);
+      gradient.addColorStop(0, "#26c6da");
+      gradient.addColorStop(1, "#00acc1");
+
+      const chartConfig = {
+        type: "bar",
+        data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [
             {
-              gridLines: { display: false },
-              ticks: { source: "labels", autoSkip: false },
-              // scaleLabel: { display: true, labelString: "Data" },
-            },
-          ],
-          yAxes: [
-            {
-              gridLines: { display: false },
-              scaleLabel: { display: true, labelString: "Notícies" },
+              label: "notícies",
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: gradient,
+              borderColor: "#00acc1",
+              borderWidth: 2,
             },
           ],
         },
-      },
-    });
-  }
-  render() {
-    return (
-      <div style={{ height: "200px", width: "70%", position: "relative" }}>
-        <canvas id="myChart" ref={this.chartRef} />
-      </div>
-    );
-  }
-}
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          title: { display: true, text: "notícies per mes" },
+          legend: { display: false },
+          scales: {
+            xAxes: [
+              {
+                gridLines: { display: false },
+                ticks: { source: "labels", autoSkip: false },
+                // scaleLabel: { display: true, labelString: "Data" },
+              },
+            ],
+            yAxes: [
+              {
+                gridLines: { display: false },
+                scaleLabel: {
+                  display: true,
+                  labelString: "Número de notícies",
+                },
+              },
+            ],
+          },
+        },
+      };
+
+      const newChartInstance = new Chartjs(chartContainer.current, chartConfig);
+      setChartInstance(newChartInstance);
+    }
+  }, [chartContainer]);
+
+  const updateDataset = (datasetIndex, newData) => {
+    chartInstance.data.datasets[datasetIndex].data = newData;
+    chartInstance.update();
+  };
+
+  const onButtonClick = () => {
+    const data = [
+      randomInt(),
+      randomInt(),
+      randomInt(),
+      randomInt(),
+      randomInt(),
+      randomInt(),
+    ];
+    updateDataset(0, data);
+  };
+
+  return (
+    <div
+      style={{
+        height: "200px",
+        width: "70%",
+        position: "relative",
+        marginTop: "20px",
+        marginBottom: "70px",
+      }}
+    >
+      <button onClick={onButtonClick}>Randomize!</button>
+      <canvas ref={chartContainer} />
+    </div>
+  );
+};
+
+export default TopicsBarChart;
