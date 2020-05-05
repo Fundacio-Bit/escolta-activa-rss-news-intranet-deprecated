add84f890c107dbdc51944388f766b7a4fcd5883
omoya_fbit <omoya@fundaciobit.org>
2020-05-05 12:12:47 +0200

    removed CleanWebpack Plugin

--- webpack_prod.config.js
+++ webpack_prod.config.js
@@ -1,17 +1,17 @@
-const webpack = require('webpack');
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const webpack = require("webpack");
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+// const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   // Options for devtool at:
   // https://webpack.js.org/configuration/devtool/
-  devtool: 'source-map',
+  devtool: "source-map",
   entry: __dirname + "/src/index.js",
   output: {
     path: __dirname + "/build",
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
 
   // https://blog.sentry.io/2015/10/29/debuggable-javascript-with-source-maps
@@ -30,30 +30,29 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
-          presets: ['es2015', 'react', 'stage-2']
-        }
+          presets: ["es2015", "react", "stage-2"],
+        },
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style', 'css')
+        loader: ExtractTextPlugin.extract("style", "css"),
       },
       // {
       //   test: /\.css$/,
       //   loader: 'style-loader!css-loader'
       // },
-    ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: __dirname + "/src/index.html"
+      template: __dirname + "/src/index.html",
     }),
     new ExtractTextPlugin("bundle.css"),
     new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
-    new CleanWebpackPlugin(["build"], { root: __dirname })
-  ]
-}
-
+    // new CleanWebpackPlugin(["build"], { root: __dirname })
+  ],
+};
 
 // solucionado quitando ExtractTextPluginLoader
