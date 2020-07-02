8797123ecfe8938add7251b28b6d4a6c27f89ee6
omoya_fbit <omoya@fundaciobit.org>
2020-07-02 14:49:20 +0200

    Removed commented code

--- webpack_dev.config.js
+++ webpack_dev.config.js
@@ -1,57 +1,43 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   // Options for devtool at:
   // https://webpack.js.org/configuration/devtool/
-  devtool: 'source-map',
+  devtool: "source-map",
   entry: __dirname + "/src/index.js",
   output: {
     path: __dirname + "/build",
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
-
-  // https://blog.sentry.io/2015/10/29/debuggable-javascript-with-source-maps
-  // devtool: "source-map",
-  // entry: {
-  //   "bundle": path.join(__dirname, 'src/index.js')
-  // },
-  // output: {
-  //   path: path.join(__dirname, 'build'),
-  //   filename: "[name].js",
-  //   sourceMapFilename: "[name].js.map"
-  // },
 
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
-          presets: ['es2015', 'react', 'stage-2']
-        }
+          presets: ["es2015", "react", "stage-2"],
+        },
       },
-      // {
-      //   test: /\.css$/,
-      //   loader: ExtractTextPlugin.extract('style', 'css')
-      // },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        loader: "style-loader!css-loader",
       },
-    ]
+      {
+        test: /\.(jpe?g|gif|png|svg|woff|ttf|wav|mp3)$/,
+        loader: "file-loader",
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: __dirname + "/src/index.html"
+      template: __dirname + "/src/index.html",
     }),
     new ExtractTextPlugin("bundle.css"),
     // new BundleAnalyzerPlugin(),
-  ]
-}
-
-
-// solucionado quitando ExtractTextPluginLoader
+  ],
+};
